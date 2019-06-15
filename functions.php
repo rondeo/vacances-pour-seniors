519a0ee0fd7657e967de416dc56bad45542f47ed
19h47 <jeremylevron@19h47.fr>
2019-06-15 20:31:18 +0200

    :rocket: WordPress theme

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Vacances Pour Seniors functions and definitions
+ *
+ * PHP version 7.2.10
+ *
+ * @package VacancesPourSeniors
+ * @since   1.0.0
+ * @link    https://github.com/19h47/vacances-pour-seniors.fr
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @see     https://developer.wordpress.org/themes/basics/theme-functions/
+ */
+
+/**
+ * Include class theme
+ *
+ * @link https://github.com/19h47/vacances-pour-seniors.fr
+ */
+require_once get_template_directory() . '/inc/class-vacancespourseniors.php';
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Index
+ *
+ * @package VacancesPourSeniors
+ * @file    index.php
+ * @author  Jérémy Levron <jeremylevron@19h47.fr> (http://19h47.fr)
+ */
+
+use Timber\Timber;
+
+if ( ! class_exists( 'Timber' ) ) {
+	echo 'Timber not activated. Make sure you activate the plugin in <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
+	return;
+}
+
+$context = Timber::context();
+
+$context['post']       = new TimberPost();
+$context['node_type']  = 'default-page';
+$context['body_class'] = 'index';
+
+$templates = array( 'index.html.twig' );
+
+Timber::render( $templates, $context );

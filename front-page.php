dacd2b1f7acfd5e3b61051a09c33ca74e0b4eda3
19h47 <jeremylevron@19h47.fr>
2019-06-17 18:35:30 +0200

    :sparkles: Front page

---
+++ front-page.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Index
+ *
+ * @package VacancesPourSeniors
+ * @file    index.php
+ * @author  Jérémy Levron <jeremylevron@19h47.fr> (http://19h47.fr)
+ */
+
+use Timber\Timber;
+
+if ( ! class_exists( 'Timber' ) ) {
+	echo 'Timber not activated. Make sure you activate the plugin in <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
+	return;
+}
+
+$context = Timber::context();
+
+$context['post']      = Timber::get_post();
+$context['pagination'] = Timber::get_pagination();
+$context['node_type']  = 'frontPage';
+$context['body_class'] = 'index';
+
+$templates = array( 'pages/front-page.html.twig' );
+
+Timber::render( $templates, $context );
